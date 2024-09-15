cff855879d4c66ce5098a46d02cdc89da45de555
kanchan gobari <gobarikanchan53@gmail.com>
2024-09-15 10:28:51 +0530

    added login button

--- src/Componenets/Header.js
+++ src/Componenets/Header.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 export const Title = () => {
     return <h1 id="title" key="h1">Food Delivery</h1>;
   };
   
   const Header = () => {
+    const [login,setLogin] = useState(true)
     return (
       <div className="nav">
         <Title />
@@ -12,6 +15,9 @@ export const Title = () => {
           <li>Contact</li>
           <li>Cart</li>
         </ul>
+        {login ? <button onClick={()=>setLogin(false)}>Login</button> : <button onClick={()=> setLogin(true)}>logout</button>}
+
+
       </div>
     );
   };

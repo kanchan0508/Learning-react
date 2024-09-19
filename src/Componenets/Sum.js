6d6541c4101ed80e478a9012537afbc4b8b6252b
kanchan gobari <gobarikanchan53@gmail.com>
2024-09-19 13:34:59 +0530

    added testing library and first test

---
+++ src/Componenets/Sum.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Sum = (num1, num2) => num1 + num2;
+  
+
+
+export default Sum
---
+++ src/Componenets/__tests__/Sum.test.js
@@ -0,0 +1,5 @@
+import Sum from "../Sum"
+
+test("check sum of positive numbers", () => {
+   expect(Sum(2, 5)).toBe(7);
+});
